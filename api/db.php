a1aa83332fb77c61bff1c254b194a4f34b28141b
Dhruv Jain <dhruv5130@outlook.com>
2026-01-25 13:49:35 +0530

    Changes to database connection

--- api/db.php
+++ api/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_hostname = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_name = "event-flow";
+$db_hostname = ;    // Define database hostname
+$db_username = ;  // Define database username
+$db_password = ;  // Define database password
+$db_name = ;    // Define database name
 
 $conn = mysqli_connect($db_hostname,$db_username,$db_password,$db_name);
 
