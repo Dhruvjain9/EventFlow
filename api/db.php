e555369448a0af926d6258869b0736b336f3ddb9
Dhruv Jain <dhruv5130@outlook.com>
2026-01-27 23:17:14 +0530

    Update db.php

--- api/db.php
+++ api/db.php
@@ -1,13 +1,15 @@
 <?php
+/*Enter your own database details*/
 
-$db_hostname = "localhost";
-$db_username = "root";
+$db_hostname = "";
+$db_username = "";
 $db_password = "";
-$db_name = "event-flow";
+$db_name = "";
 
 $conn = mysqli_connect($db_hostname,$db_username,$db_password,$db_name);
 
 if($conn-> connect_error){
     echo connection_status;
 }
+
 ?>
